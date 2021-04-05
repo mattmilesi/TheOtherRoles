dc16df06bf90421ce6dc300c4817f8c00d436156
Matteo Milesi <matteo.milesi@hotmail.it>
2021-04-05 22:19:22 +0200

    Change to credits

--- Source Code/CredentialsPatch.cs
+++ Source Code/CredentialsPatch.cs
@@ -12,7 +12,7 @@ namespace TheOtherRoles
     {
         static void Postfix(VersionShower __instance) {
             string spacer = new String('\n', 15);
-            string text = "[FCCE03FF]MMod[] v1.0.0:\n- Modded by [FCCE03FF]mattmilesi[] and [FFEB91FF]maramerda[]\n- Based on [FFEB91FF]TheOtherRoles";
+            string text = "[FCCE03FF]MadMod [] v1.0.0:\n- Modded by [FCCE03FF]mattmilesi[] and the [FFEB91FF]MadBit Coding GVNG[]\n- Based on [FFEB91FF]TheOtherRoles";
             if (__instance.text.Text.Contains(spacer))
                 __instance.text.Text += "\n" + text;
             else
@@ -25,8 +25,8 @@ namespace TheOtherRoles
     {
         static void Postfix(VersionShower __instance)
         {
-            __instance.text.Text += "\n[FCCE03FF]MMod[FFFFFFFF]";
-            __instance.text.Text += "\nModded by [FCCE03FF]mattmilesi[FFFFFFFF]";
+            __instance.text.Text += "\n[FCCE03FF]MadMod[FFFFFFFF]";
+            __instance.text.Text += "\nModded by the [FCCE03FF]MadBit Coding GVNG[FFFFFFFF]";
         }
     }
 }
